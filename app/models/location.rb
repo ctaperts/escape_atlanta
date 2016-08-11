bad8b501a629fe378aeca5bbef1b15ec9b640d12
Colby Taperts <colbytaperts@gmail.com>
2016-08-11 09:31:41 -0500

    setup scaffold and geocoder in model

---
+++ app/models/location.rb
@@ -0,0 +1,9 @@
+class Location < ApplicationRecord
+  geocoded_by :full_street_address   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
+
+  def full_street_address
+    "#{street_address}, #{city}, GA"
+  end
+
+end
